30336976f93d3b9808f677f4d3c2d726333c67c5
B112Spring0ne <1780747613@qq.com>
2018-05-28 23:14:51 +0800

    delete

--- b112-twly-dao/src/main/java/edu/zut/cs/javaee/dream/admin/dao/ProprieterDao.java
+++
@@ -1,8 +0,0 @@
-package edu.zut.cs.javaee.dream.admin.dao;
-
-import edu.zut.cs.javaee.dream.admin.domain.Proprieter;
-import edu.zut.cs.javaee.dream.base.dao.GenericDao;
-
-public interface ProprieterDao extends GenericDao<Proprieter, Long> {
-	
-}
