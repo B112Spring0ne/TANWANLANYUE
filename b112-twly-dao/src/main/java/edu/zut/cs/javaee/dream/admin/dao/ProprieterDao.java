41dbf2fc1ca10d727a262a37fa79b2284c353e2b
B112Spring0ne <1780747613@qq.com>
2018-05-28 23:19:45 +0800

    update

---
+++ b112-twly-dao/src/main/java/edu/zut/cs/javaee/dream/admin/dao/ProprieterDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.javaee.dream.admin.dao;
+
+import edu.zut.cs.javaee.dream.admin.domain.Proprieter;
+import edu.zut.cs.javaee.dream.base.dao.GenericDao;
+
+public interface ProprieterDao extends GenericDao<Proprieter, Long> {
+	
+}
