55d6109e3ed3b23556c0d8e7f7cf07efa49b53bd
LiuQQQi <2723340391@qq.com>
2018-06-14 21:20:40 +0800

    update

---
+++ b112-twly-service/src/main/java/edu/zut/cs/javaee/dream/admin/service/TeacherManager.java
@@ -0,0 +1,8 @@
+package edu.ztu.cs.assn.service;
+
+import edu.zut.cs.javaee.dream.admin.domain.Teacher;
+import edu.zut.cs.javaee.dream.base.service.GenericManager;
+
+public interface TeacherManager extends GenericManager<Teacher,Long>{
+
+}
