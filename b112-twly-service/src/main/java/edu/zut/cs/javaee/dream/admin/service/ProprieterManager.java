7bc63d65d2ee831002302d114342c77f3c2ae5b4
B112Spring0ne <1780747613@qq.com>
2018-06-15 09:06:27 +0800

    upload

---
+++ b112-twly-service/src/main/java/edu/zut/cs/javaee/dream/admin/service/ProprieterManager.java
@@ -0,0 +1,15 @@
+package edu.zut.cs.javaee.dream.admin.service;
+
+import java.util.List;
+
+import edu.zut.cs.javaee.dream.admin.domain.Proprieter;
+import edu.zut.cs.javaee.dream.base.service.GenericManager;
+
+
+
+public interface ProprieterManager extends GenericManager<Proprieter, Long>{
+
+	List<Proprieter> findAll();
+
+	Proprieter findbyUsername(String Proprieter);
+}
