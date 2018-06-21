86281e5a58bdd8673b6ae4b9900cd97f19061945
liuzhongya <1225801928@qq.com>
2018-06-21 14:29:59 +0800

    gai shiti

--- b112-twly-dao/src/main/java/edu/zut/cs/javaee/dream/admin/domain/SheYuan.java
+++ b112-twly-dao/src/main/java/edu/zut/cs/javaee/dream/admin/domain/SheYuan.java
@@ -2,64 +2,50 @@ package edu.zut.cs.javaee.dream.admin.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import edu.zut.cs.javaee.dream.base.domain.BaseEntity;
 
-@Table(name = "T_ADMIN_USER")
-@Entity
-public class SheYuan extends BaseEntity {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -4376674977047164142L;
-
-	@Column(name = "USERNAME")
-	String username;
-
-	@Column(name = "PASSWORD")
+@Table(name="bmyorg")
+public class SheYuan extends BaseEntity{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "password")
 	String password;
-
-	@Column(name = "EMAIL")
+	
+	@Column(name = "usename")
+	String name;
+	
+	@Column(name = "mass")
+	String post;
+	
+	@Column(name = "Email")
 	String email;
-
-	public Group getGroup() {
-		return group;
-	}
-
-	public void setGroup(Group group) {
-		this.group = group;
-	}
-
-	@ManyToOne
-	@JoinColumn(name = "GROUP_ID")
-	Group group;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
 
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPost() {
+		return post;
+	}
+	public void setPost(String post) {
+		this.post = post;
+	}
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	
 }
