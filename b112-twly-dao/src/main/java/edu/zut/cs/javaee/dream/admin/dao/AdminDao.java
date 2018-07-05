9a63f99fc5422b84c6dea6ecdc421f94e504c7cd
B112Spring0ne <1780747613@qq.com>
2018-07-05 12:26:42 +0800

    clear

--- b112-twly-dao/src/main/java/edu/zut/cs/javaee/dream/admin/dao/AdminDao.java
+++
@@ -1,9 +0,0 @@
-package edu.zut.cs.javaee.dream.admin.dao;
-
-
-import edu.zut.cs.javaee.dream.admin.domain.Admin;
-import edu.zut.cs.javaee.dream.base.dao.GenericDao;
-
-public interface AdminDao extends GenericDao<Admin, Long>{
-
-}
--- b112-twly-dao/src/main/java/edu/zut/cs/javaee/dream/admin/dao/FinanceDao.java
+++
@@ -1,8 +0,0 @@
-package edu.zut.cs.javaee.dream.admin.dao;
-
-import edu.zut.cs.javaee.dream.admin.domain.Finance;
-import edu.zut.cs.javaee.dream.base.dao.GenericDao;
-
-public interface FinanceDao extends GenericDao<Finance, Long> {
-	
-}
